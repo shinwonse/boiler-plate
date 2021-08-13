490f6b2901fe67f12acd4aeb752ea9f4ddc60815
wonse <sinwonse@gmail.com>
2021-08-14 00:03:00 +0900

    Auth 기능 만들기

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express'); // express 모듈을 가져오고
 const app = express(); // express function을 이용해서 새로운 express app을 만들고
 const port = 5000; // port는 아무 port나 가능
 const bodyParser = require('body-parser'); // body-parser 모듈을 가져옴
-
+const cookieParser = require('cookie-parser');
 const config = require('./config/key'); // key.js 가져옴
-
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User"); // User Schema를 가져옴
 
 // application/x-ww-from-urlencoded
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 // application/json
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
@@ -41,11 +41,11 @@ app.post('/register', (req,res) => { // 회원가입을 위한 route
   })
 })
 
-app.post('/login', (req,res) => {
+app.post('/api/users/login', (req,res) => {
 
   // 요청된 이메일을 데이터베이스에서 있는지 찾는다
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if(!userInfo) {
+  User.findOne({ email: req.body.email }, (err, user) => { // mongoDB 메소드 findOne
+    if(!user) {
       return res.json({
         loginSuccess: false,
         message: "제공된 이메일에 해당하는 유저가 없습니다."
@@ -58,9 +58,33 @@ app.post('/login', (req,res) => {
 
       // 비밀번호까지 맞다면 토큰을 생성하기.
       user.generateToken((err, user) =>{
-        
-      })
+        if(err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지 ...
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess:true, userId: user._id}) 
+      })  
     })
+  })
+
+  // role 1 어드민 role 2 특정 부서 어드민
+  // role 0 -> 일반유저 role 0이 아니면 관리자
+
+  app.get('/api/users/auth',auth,(req,res) => {
+
+    // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말
+    res.status(200).json({
+      _id: req.user._id,
+      isAdmin: req.user.role === 0 ? false : true,
+      isAuth: true,
+      email: req.user.email,
+      name: req.user.name,
+      lastname: req.user.lastname,
+      role: req.user.role,
+      image: req.user.image
+    })
+
   })
 
   
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -56,9 +57,43 @@ userSchema.pre('save', function(next){
 
 userSchema.methods.comparePassword = function(plainPassword, cb) {
     // plainPassword 1234567    암호화된 비밀번호
-    bcrtpy.compare(plainPassword, this.password, function(err, isMatch){
-        if(err) return cb(err),
-            cb(null, isMatch)
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){ // plainPassword를 암호화해서 비교를한다.
+        if(err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+
+    var user = this;
+
+    // jsonwebtoken을 이용해서 token을 생성하기
+
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    // user._id + 'secretToken' = token
+    // 'secretToekn' -> user._id
+
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err) // 만약 err 발생시 err 를 콜백해주고
+        cb(null, user) // 에러가 발생하지 않았다면 콜백에서 err 는 null 이고 user 정보를 전달해줌
+    })
+}
+
+userSchema.statics.findByToken = function(token,cb) {
+    var user = this;
+
+    // 토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err,decoded){
+        // 유저 아이디를 이용해서 유저를 찾은 다음에
+        // 클라이언트에서 가져오 token과 DB에 보관된 토큰이 일치하는지 확인
+
+        user.findOne({"_id": decoded, "token": token}, function(err,user){ // mongoDB 메소드 id와 token 이용
+
+            if(err) return cb(err);
+            cb(null,user)
+        })
     })
 }
 
