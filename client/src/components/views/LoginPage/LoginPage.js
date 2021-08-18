717402b7bf109ab82866b9a1793b7a09ea28cbda
wonse <sinwonse@gmail.com>
2021-08-19 00:00:44 +0900

    로그인 페이지 구현

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import axios from 'axios'
+// import { response } from 'express'
+import { useDispatch } from 'react-redux'
+import { loginUser } from '../../../_actions/user_actions';
+import React, {useState} from 'react'
 
 function LoginPage() {
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        console.log('Email', Email)
+
+        dispatch(loginUser(body))
+    }
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display:'flex', justifyContent:'center',alignItems:'center'
+            ,width:'100%',height:'100vh'
+        }}>
+
+            <form style={{display:'flex', flexDirection: 'column'}}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                <br />
+                <button>
+                    Login
+                </button>
+            </form>
+
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -14,11 +14,11 @@ const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)
 
 ReactDOM.render(
 
-  <Provider>
+  <Provider
     store={createStoreWithMiddleware(Reducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-      )}
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+  )}>
     <App/>
   </Provider>
 
